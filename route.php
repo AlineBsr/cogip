b8896416eb8ca111f1ed567cd9f9f09164741267
Mess Dorchies <messaouddorchies@gmail.com>
2021-12-01 09:17:23 +0100

    CommitFirst

---
+++ route.php
@@ -0,0 +1,34 @@
+<?php 
+
+    // var_dump($_GET);
+
+    define('ROOT', str_replace('route.php', '' , $_SERVER['SCRIPT_FILENAME']));
+
+    $parameters = explode('/', $_GET['u']);
+
+    // var_dump($parameters);
+
+    if($parameters[0] != ""){
+
+        $controller = ucfirst($parameters[0]);
+        $action = isset($parameters[1]) ? $parameters[1] : 'route';
+
+        require_once(ROOT.'controllers/'.$controller.'.php');
+
+        $controller = new $controller();
+
+        if(method_exists($controller,$action)){
+            $controller->$action();
+        } 
+        else{
+            http_response_code(404);
+            echo "Impossible de trouver l'action demandé.";
+        } 
+    }
+    else {
+        http_response_code(404);
+        echo "Impossible de trouver la page demandé.";
+    }
+
+    // var_dump($parameters[0]);
+    // var_dump($parameters[1]);
