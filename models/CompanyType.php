636637278170766a6991075d03b1a9b5fab43ade
Mess Dorchies <messaouddorchies@gmail.com>
2021-12-07 14:09:17 +0100

    CommitTuesdayMidDay

--- models/CompanyType.php
+++ models/CompanyType.php
@@ -7,4 +7,18 @@
             $this->table = "company_type";
             $this->getConnection();
         }
+
+        public function getAllClient(string $table2, string $condition){
+            $sql = "SELECT * FROM " .$this->table." LEFT JOIN ".$table2. " ON ". $this->table.".".$condition." = ". $table2.".".$condition. " WHERE " .$this->table. ".type = 'Client'";
+            $query = $this->connectionString->prepare($sql);
+            $query->execute();
+            return $query->fetchall();
+        }
+
+        public function getAllProvider(string $table2, string $condition){
+            $sql = "SELECT * FROM " .$this->table." LEFT JOIN ".$table2. " ON ". $this->table.".".$condition." = ". $table2.".".$condition. " WHERE " .$this->table. ".type = 'Provider'";
+            $query = $this->connectionString->prepare($sql);
+            $query->execute();
+            return $query->fetchall();
+        }
     }
