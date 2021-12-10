815dc4edc55d2fcb359f390834960014356f949a
AlineBsr <bosser.aline@gmail.com>
2021-12-10 16:20:28 +0100

    fix layout + links nav

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -1,4 +1,3 @@
-<html>
     <head>
     <link rel=”stylesheet” href=”https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css”rel=”nofollow” integrity=”sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I” crossorigin=”anonymous”>
         <link rel="stylesheet" href="../global/style/default.css">
@@ -10,11 +9,11 @@
             <div class="navbar">
                 <a class="logo" href="#"> COGIP</a>
                 <ul class="nav">
-                    <li><a href="#">Accueil</a></li>
-                    <li><a href="#">Factures</a></li>
-                    <li><a href="#">Sociétés</a></li>
-                    <li><a href="#">Contacts</a></li>
-                    <li><a href="#">Admin</a></li>
+                    <li><a href="../AdminController/welcome">Accueil</a></li>
+                    <li><a href="../InvoiceController/allinvoices">Factures</a></li>
+                    <li><a href="../CompanyController/listAll">Sociétés</a></li>
+                    <li><a href="../PeopleController/getPeople">Contacts</a></li>
+                    <li><a href="../AdminController/welcome">Admin</a></li>
                 </ul>
             </div>
         </header>
@@ -29,4 +28,3 @@
             <h3>Vive la cogip!</h3>
         </footer>
     </body>
-</html>
